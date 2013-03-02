0212c62be27e3aa1e8e7dd94ad98251d930d1d1e
Ando <thelegendofando@googlemail.com>
2013-03-02 23:40:47 +0000

    add IEnemy interface

---
+++ Units/Abstractions/IEnemy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowerDefence.Units
+{
+    public interface IEnemy
+    {
+        int HitPoints;
+        int LifeReduction;
+        int CurrencyReward;
+        int Speed;
+    }
+}
